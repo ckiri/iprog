2e41927324af81227220801c3a46bc50812bf4da
Chris Kiriakou <chris@debian-BULLSEYE-live-builder-AMD64>
2021-11-08 19:35:12 +0100

    Changed a typo

--- src/first/TryMyIO.java
+++ src/first/TryMyIO.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class TryMyIO {
 	  public static void main (String[] arg) {
 
-	    String line = MyIO.promptAndRead("Gib mir ein Wort: ");
+	    String line = MyIO.promtAndRead("Gib mir ein Wort: ");
 	    MyIO.writeln("Es wurde \"" + line + "\" engegeben.");
 
 	    BigInteger i = MyIO.readBigInteger("Gib mir eine zahl: ");
