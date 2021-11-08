a6d20f2d1d5a578adcd8652cc4438e5a8112ae9c
agres <agres.dominik@gmail.com>
2021-11-08 10:00:11 +0100

    :)

--- src/first/MyMath.java
+++ src/first/MyMath.java
@@ -23,8 +23,8 @@ public class MyMath {
     }
     public static void main (String[] args) {
         int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
-        System.out.println("Die Summe von "+catAll(array)+" ergibt "+addAll(array)+".");
-        System.out.println("Das Produkt von "+catAll(array)+" ergibt "+multiplyAll(array)+"." );
+        System.out.println("Die Summe von "+catAll(array)+" ergibt "+addAll(array)+ "");
+        System.out.println("Das Produkt von "+catAll(array)+" ergibt "+multiplyAll(array)+ "");
         System.out.println("Die Aufruf-Argumente sind: "+catAll(array));
     }
 }
