e02f0f63f13a0c1f464b8071b2aab576b6901697
agres <agres.dominik@gmail.com>
2021-11-08 18:49:47 +0100

    08.11.21

--- src/first/EsreverLister.java
+++ src/first/EsreverLister.java
@@ -2,31 +2,37 @@ package first;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class EsreverLister {
 
     public static String invert (String x)
     {
         return x.length() <= 1 ? x :
-                invert( x.substring(1) ) + x.substring(0,1);
+                invert( x.substring(1) ) + x.substring(0,1);            //Invert einer Line/Zeile
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Ende mit potS.");
-        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-        String array[];
-        for (int i = 0;i<100;i++){
-            System.out.println("> ");
-            // String s = keyboard.readLine();
-            array[i] = keyboard.readLine();
-            if (array[i] == null || array[i].equalsIgnoreCase("potS")){
-                break;
+        System.out.println("Ende mit potS oder ^Z.");                                        //Message beim aufruf
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));      //Input reader
+        List<String> WordList = new ArrayList<String>();                                     //ArrayList (s.38 - 43)
+        for (;;){                                                                            //Always True for-schleife
+            System.out.println("-> ");
+            String s = keyboard.readLine();                                                  //Read Line String
+            if (s == null || s.equalsIgnoreCase("potS")){
+                break;                                                                       //If Null dann beenden
             }
-            // array[i] = keyboard.readLine();
+            WordList.add(invert(s));                                                         //Hier wird die Invertierte String S zur WorteListe geaddet
+            }
 
-        }
-        System.out.println(invert(Arrays.toString(array)));
+            ListIterator<String> x = WordList.listIterator(WordList.size());                 //nuer Iterator x mit worterliste
+            while(x.hasPrevious()){
+                System.out.println(x.previous());
+            }                                                                                //While x noch zeilen hat dann drückt er sie
+
+        System.out.println("Ende.");                                                         //Schluss ende
         keyboard.close();
     }
 }
