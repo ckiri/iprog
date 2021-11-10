5ffc42d4621ad99656cb2d605a845ac3895a63ef
denisthefirst <93917742+denisthefirst@users.noreply.github.com>
2021-11-10 11:37:16 +0100

    Update Sinus.java

--- src/first/Sinus.java
+++ src/first/Sinus.java
@@ -1,7 +1,7 @@
 package first;
 
 /**
- * das die sin-Funktion f�r einen eingelesenen x-Wert entsprechend dieser Summe approximiert und ausgibt:
+ * dass die sin-Funktion für einen eingelesenen x-Wert entsprechend dieser Summe approximiert und ausgibt:
  * @author DDeifel, DMaric, DAgres, CKiriakou
  * @version 1, 08.11.2021
  */
@@ -30,29 +30,25 @@ public class Sinus
     {
         int k = 0;      //k Variable
         int maxIterations = 10000;   //Parameter maxIteration
+        double maxError = 1e-10;        //Parameter max error
+        double xn = x;                  
+        double xnPlus1 = 0;
         
         double eErg = 0;
 		x = x - Pi(eErg )*2 * ((long)(x/(Pi(eErg)*2)));
-        
-        double maxError = 1e-10;        //Parameter max error
-        double xn = x;                  //(long) redundant, x ist long
-        double xnPlus1 = 0;
-        
 
-        
-         do {  xnPlus1 +=xn;
+        do {  xnPlus1 +=xn;
             k++;
             xn = -xn * x * x / (2.0 * k * (2.0 * k + 1.0));     //xnPlus1 = formel ?  x ist long / Zahlen und k sind int
         }
          while(Math.abs(xn) >= maxError && k <= maxIterations); 
-        return xnPlus1 + xn;     //return ???
+        return xnPlus1 + xn;     //return 
     }
 
     public static void main(String[] args) 
     {
         long x = MyIO.readLong("x = ");
-        System.out.println(sin(x));
-        
-
+        System.out.println("Lösung: " + sin(x));
+    
     }   
 }
