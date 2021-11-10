9834702b18e01440bbe323ff8e7762e096d84595
denisthefirst <93917742+denisthefirst@users.noreply.github.com>
2021-11-10 11:18:37 +0100

    Add files via upload

--- src/first/Sinus.java
+++ src/first/Sinus.java
@@ -1,42 +1,58 @@
 package first;
 
 /**
- * das die sin-Funktion für einen eingelesenen x-Wert entsprechend dieser Summe approximiert und ausgibt:
+ * das die sin-Funktion f�r einen eingelesenen x-Wert entsprechend dieser Summe approximiert und ausgibt:
  * @author DDeifel, DMaric, DAgres, CKiriakou
  * @version 1, 08.11.2021
  */
 
 public class Sinus 
 {
-    public static double sin(long x)
+	public static double Pi(double eErg) {
+		long n = 1000000;
+		long i = 1L;
+		double j = 1;
+		double formel;
+		double zErg = 0;
+		
+		// Beginn der While-Schleife
+		while(i <= n) {
+			formel = 6/(j*j);
+			i++;
+			j++;			
+			zErg = zErg + formel;  
+		}
+		return  eErg = Math.sqrt(zErg);
+		
+	}
+
+    public static double sin(double x)
     {
-        int k = 1;      //k Variable
-        int n = 0;      //Zähler Iteration
-        double maxIterations = 10000;   //Parameter maxIteration
+        int k = 0;      //k Variable
+        int maxIterations = 10000;   //Parameter maxIteration
+        
+        double eErg = 0;
+		x = x - Pi(eErg )*2 * ((long)(x/(Pi(eErg)*2)));
+        
         double maxError = 1e-10;        //Parameter max error
         double xn = x;                  //(long) redundant, x ist long
-        double xnPlus1 = x;
+        double xnPlus1 = 0;
+        
 
-        int i = 0;  //Zähler
-
-        //Hier: xn und xnPlus1 haben den Wert x
-
-        do{
-            xn = xnPlus1;
-            xnPlus1 = xn + ( (-1) * x * x / (2*k) / ((2 * k) +1));     //xnPlus1 = formel ?  x ist long / Zahlen und k sind int
-            n++; k++;
-
-            System.out.println(i); i++;         //Zähler, später löschen
-
-        } while(Math.abs(xnPlus1-xn) >= maxError && n < maxIterations);
-
-        return xnPlus1;     //return ???
+        
+         do {  xnPlus1 +=xn;
+            k++;
+            xn = -xn * x * x / (2.0 * k * (2.0 * k + 1.0));     //xnPlus1 = formel ?  x ist long / Zahlen und k sind int
+        }
+         while(Math.abs(xn) >= maxError && k <= maxIterations); 
+        return xnPlus1 + xn;     //return ???
     }
 
     public static void main(String[] args) 
     {
         long x = MyIO.readLong("x = ");
         System.out.println(sin(x));
+        
 
     }   
 }
