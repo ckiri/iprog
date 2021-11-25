21b85e7da15dea57cff5ae75ddda6b0958288cc0
Dennis <93908898+Dennisdude@users.noreply.github.com>
2021-11-25 11:10:19 +0100

    Add files via upload

--- src/first/Fraction.java
+++ src/first/Fraction.java
@@ -2,6 +2,11 @@ package first;
 
 import java.math.BigInteger;
 
+/**
+ * @author DDeifel, DMaric, DAgres, CKiriakou
+ * @version 1, 24.11.2021
+ */
+
 public class Fraction extends Number implements Comparable<Fraction>
 {
     private BigInteger zaehler;
@@ -23,15 +28,80 @@ public class Fraction extends Number implements Comparable<Fraction>
         return this.zaehler;    
     }
 
+    public Fraction add(Fraction o) 
+    {
+        Fraction x = new Fraction(BigInteger.ONE, BigInteger.ONE);
+        if ( nenner.equals(o.nenner) )
+        {
+            x.nenner = nenner;
+            x.zaehler = zaehler.add(o.zaehler);
+        }
+        else
+        {
+            x.nenner = getKgV( nenner, o.nenner);
+            x.zaehler = zaehler.multiply(nenner.divide(x.nenner)).add(o.zaehler.multiply(o.nenner.divide(x.nenner)));
+        }
+        return x;
+    }
+
+    public Fraction subtract(Fraction o) 
+    {
+        Fraction x = new Fraction(BigInteger.ONE, BigInteger.ONE);
+        if ( nenner.equals(o.nenner) )
+        {
+            x.nenner = nenner;
+            x.zaehler = zaehler.add(o.zaehler);
+        }
+        else
+        {
+            x.nenner = getKgV( nenner, o.nenner);
+            x.zaehler = zaehler.multiply(nenner.divide(x.nenner)).subtract(o.zaehler.multiply(o.nenner.divide(x.nenner)));
+        }
+        return x;
+    }
+
+    public static BigInteger getKgV (BigInteger x, BigInteger y) 
+    {
+        return ( x.multiply( y.divide( x.gcd(y) )));
+    }
+
+    public Fraction multiply(Fraction o) 
+    {
+        Fraction x = new Fraction(BigInteger.ONE, BigInteger.ONE);
+        
+        x.nenner = nenner.multiply(o.nenner);
+        x.zaehler = zaehler.multiply(o.zaehler);
+
+        x.nenner = x.nenner.divide(x.zaehler.gcd(x.nenner));
+        x.zaehler = x.zaehler.divide(x.zaehler.gcd(x.nenner));
+
+        return x;
+    }
+    
+    public Fraction divide(Fraction o) 
+    {
+        Fraction x = new Fraction(BigInteger.ONE, BigInteger.ONE);
+        
+        x.nenner = nenner.multiply(o.zaehler);
+        x.zaehler = zaehler.multiply(o.nenner);
+
+        x.nenner = x.nenner.divide(x.zaehler.gcd(x.nenner));
+        x.zaehler = x.zaehler.divide(x.zaehler.gcd(x.nenner));
+
+        return x;
+    }
+
+    public boolean isInteger()
+    {
+        BigInteger x = BigInteger.valueOf(0);
+        return zaehler.mod(nenner).equals(x);
+    }
+
     @Override
     public String toString() 
     {
-        return this.zaehler + "/" + this.nenner;
-    }
-
-    public boolean isInteger(BigInteger a, BigInteger b){
-        BigInteger x = BigInteger.valueOf(0);
-        return a.mod(b).equals(x);
+        if ( nenner.equals(BigInteger.ONE) ) return zaehler + ""; 
+        else return zaehler + "/" + nenner;
     }
 
     @Override
@@ -58,11 +128,27 @@ public class Fraction extends Number implements Comparable<Fraction>
         return (zaehler.divide(nenner)).doubleValue();
     }
 
+    /**
+     * @return -1, 0 or 1 as this Fraction is numerically less than, equal
+     *         to, or greater than other Fracton.
+     */
     @Override
-    public int compareTo(Fraction o) 
+    public int compareTo(Fraction other) 
     {
-        return 0;
+        if ( ((this.zaehler).divide(this.nenner)).equals( other.zaehler.divide(other.nenner)) )
+        {
+            return 0;
+        }
+        else
+        {
+            if (zaehler.divide(nenner) == zaehler.divide(nenner).max( other.zaehler.divide(other.nenner) ))
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
     }
-
-
 }
