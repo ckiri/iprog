bc3ecac57fef90f1adbf8ff39bf1782d1ae96c55
denisthefirst <93917742+denisthefirst@users.noreply.github.com>
2021-12-06 19:50:00 +0100

    Add files via upload

--- src/first/MathPlotter.java
+++ src/first/MathPlotter.java
@@ -3,100 +3,86 @@ package first;
 public class MathPlotter {
 	
 	
-		static String funktion = MyIO.promtAndRead("Wählen Sie eine dieser Funktionen aus: sin, cos, exp, log, sqrt, tan, square, cube, quad oder tower: ");
-
-	
-	static double b = MyIO.readDouble("Wert Beginn: ");
-	static double i = MyIO.readDouble("Wert Schrittweite: ");
-	static double e = MyIO.readDouble("Wert Ende: ");
+	static String funktion;
+	static double b;
+	static double i;
+	static double e;
 	static double a;
 
 	public static void plot(D2Method meth) {
 		for(; b<=e; b += i) {
 				a = b;
-				MyIO.writeln(b + "-> " +meth.compute(a));
-			
+				MyIO.writeln(b + "--> " +meth.compute(a));
 		}
 	}
-	
-	
-
 	public static void main(String[] args) throws Exception {
 		
 		while(true) {
-			if(funktion.equals("sin")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return Math.sin(Math.toRadians(a));						
-					}
-				});
+			funktion = MyIO.promtAndRead("Wählen Sie eine dieser Funktionen aus: sin, cos, exp, log, sqrt, tan, square, "
+					+ "cube, quad oder tower ('stop' eingeben um das Programm zu beenden): ");
+			
+			if(funktion.equals("stop")) {
+				System.out.println("Sie haben das Programm beendet. Auf Wiedersehen :)");
+				break;
 			}
-			else if(funktion.equals("cos")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return Math.cos(Math.toRadians(a));						
-					}
-				});
-			}
-			else if(funktion.equals("exp")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return Math.exp(a);						
-					}
-				});
-			}
-			else if(funktion.equals("log")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return Math.log10(a);						
-					}
-				});
-			}
-			else if(funktion.equals("sqrt")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return Math.sqrt(a);						
-					}
-				});
-			}
-			else if(funktion.equals("tan")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return Math.tan(a);						
-					}
-				});
-			}
-			else if(funktion.equals("square")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return a*a;						
-					}
-				});
-			}
-			else if(funktion.equals("cube")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return a*a*a;						
-					}
-				});
-			}
-			else if(funktion.equals("quad")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									return a*a*a*a;						
-					}
-				});
-			}
-			else if(funktion.equals("tower")) {
-				plot(new D2Method(){
-								public double compute(double a) {
-									double s = a;
-									double x = s*(a*a);
-									return x*x;
-								
-					}
-				});
-			}
-		}
-	}	
+			b = MyIO.readDouble("Wert Beginn: ");
+			i = MyIO.readDouble("Wert Schrittweite: ");
+			e = MyIO.readDouble("Wert Ende: ");
+			
+			switch(funktion) {
+			case "sin" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.sin(a);	
+				}
+			});	break;
+			case "cos" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.cos(a);						
+				}
+			}); break;
+			case "exp" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.cos(Math.toRadians(a));						
+				}
+			}); break;
+			case "log" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.log(a);						
+				}
+			}); break;
+			case "sqrt" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.sqrt(a);						
+				}
+			}); break;
+			case "tan" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.tan(a);						
+				}
+			}); break;
+			case "square" : plot(new D2Method(){
+				public double compute(double a) {
+					return a*a;						
+				}
+			}); break;
+			case "cube" : plot(new D2Method(){
+				public double compute(double a) {
+					return a*a*a;						
+				}
+			}); break;
+			case "quad" : plot(new D2Method(){
+				public double compute(double a) {
+					return a*a*a*a;						
+				}
+			}); break;
+			case "tower" : plot(new D2Method(){
+				public double compute(double a) {
+					return Math.pow(a, Math.pow(a, a));						
+				}
+			}); break;
+			default: System.out.println("Diese Funktion existiert nicht: " + funktion);
+			}	  
+		}	
+	}
 }
+
