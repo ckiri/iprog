fc36ec21f3e1f53f983827a3d70945551a52d271
denisthefirst <93917742+denisthefirst@users.noreply.github.com>
2021-12-07 00:03:41 +0100

    Add files via upload

--- src/first/MathPlotter.java
+++ src/first/MathPlotter.java
@@ -1,5 +1,21 @@
 package first;
 
+/**
+ *Der Benutzer wird aufgefordert, den Namen einer der (in der Klasse Math 
+ *vorhandenen) mathematischen Funktionen sin, cos, exp, log, 
+ *sqrt, tan oder aber square , cube , quad und tower (für die 
+ *Berechnung von Potenzen x^x^x) einzugeben. 
+ *Wurde eine Funktion eingegeben, sollen Zahlwerte für Beginn, Schrittweite, 
+ *und Ende erfragt und eingelesen und anschließend die Funktion geplottet 
+ *(ausgegeben) werden.
+ *Danach kann gleich wieder die nächste Funktion geplottet werden (Fortsetzung 
+ *also bei Schritt 1). Nur wenn statt des Funktionsnamens stop eingegeben 
+ *wurde, soll das Programm enden.
+
+ * @author Dennis Deifel, Denis Maric, Dominik Agres, Chris Kiriakou
+ * @version 1, 06.12.2021
+ */
+
 public class MathPlotter {
 	
 	
@@ -17,18 +33,25 @@ public class MathPlotter {
 	}
 	public static void main(String[] args) throws Exception {
 		
+		//Anfang der While schleife
 		while(true) {
+			
+			//Einlesen der beliebigen Funktion
 			funktion = MyIO.promtAndRead("Wählen Sie eine dieser Funktionen aus: sin, cos, exp, log, sqrt, tan, square, "
 					+ "cube, quad oder tower ('stop' eingeben um das Programm zu beenden): ");
 			
+			//wenn stop eingegeben ist, hört das Programm auf
 			if(funktion.equals("stop")) {
 				System.out.println("Sie haben das Programm beendet. Auf Wiedersehen :)");
 				break;
 			}
+			
+			//Einlesen von beginn, schrittweite, ende
 			b = MyIO.readDouble("Wert Beginn: ");
 			i = MyIO.readDouble("Wert Schrittweite: ");
 			e = MyIO.readDouble("Wert Ende: ");
 			
+			//switch cases
 			switch(funktion) {
 			case "sin" : plot(new D2Method(){
 				public double compute(double a) {
@@ -80,6 +103,8 @@ public class MathPlotter {
 					return Math.pow(a, Math.pow(a, a));						
 				}
 			}); break;
+			
+			//ungültige Eingabe, nochmal fragen
 			default: System.out.println("Diese Funktion existiert nicht: " + funktion);
 			}	  
 		}	
